0ddaad98630c12650232d66f2a168818719b9731
cameronduffin <camduffin@gmail.com>
2021-03-22 18:12:48 -0400

    adjusting scroll behaviour, and adding animations

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const app = {};
 
+// const app.lazyScroll = () => {
+
+//     const downArrow = document.getElementsByClassName('down-arrow');
+
+//     downArrow.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         const aboutMe = document.getElementById('about-me');
+    
+//         aboutMe.scrollIntoView({behavior: smooth});
+//     })
+
+// }
+
+
 app.init = () => {
 
 }
