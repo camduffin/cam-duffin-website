35c8647b82329a3f9618a73c44816f6d2bf82ddc
cameronduffin <camduffin@gmail.com>
2021-03-25 14:35:40 -0400

    ensuring page meets all accessibility standards

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 const app = {};
 
-let mainNavLinks = document.querySelectorAll("aside nav ul li a");
-let mainSections = document.querySelectorAll("main section");
-
-let lastId;
-let cur = [];
 
 app.activeHighlight = () => {
 
+    let mainNavLinks = document.querySelectorAll("aside nav ul li a");
+    let mainSections = document.querySelectorAll("main section");
+
+    let lastId;
+    let cur = [];
+
     window.addEventListener("scroll", event => {
         let fromTop = window.scrollY;
-    
+
         mainNavLinks.forEach(link => {
             let section = document.querySelector(link.hash);
-    
+
             if (
-            section.offsetTop <= fromTop &&
-            section.offsetTop + section.offsetHeight > fromTop
+                section.offsetTop <= fromTop &&
+                section.offsetTop + section.offsetHeight > fromTop
             ) {
-            link.classList.add("current");
+                link.classList.add("current");
             } else {
-            link.classList.remove("current");
+                link.classList.remove("current");
             }
         });
     });
 }
-
-
 
 app.init = () => {
     app.activeHighlight();
